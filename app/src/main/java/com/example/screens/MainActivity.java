e937b216a86ca1ef15220120e2c1511c4918cbe2
Emil Jonsson <d21emijo@student.his.se>
2022-05-05 16:29:09 +0200

    ändrat build.gradles compileSdkVersion 31 och targetSdkVersion 31 från version 30 till version 31

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -10,21 +10,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView login = findViewById(R.id.logIn);
-    Button loginbutton = findViewById(R.id.logInButton);
-    TextView user = findViewById(R.id.user);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("===","knapp tryckt");
-            }
-        });
 
     }
 }
