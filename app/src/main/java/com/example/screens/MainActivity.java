53ae5488814362bd609eb44293eea3283c14ef1b
Emil Jonsson <d21emijo@student.his.se>
2022-05-05 17:13:07 +0200

    knapp+text in MainActivity + gettext from user

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -12,11 +12,29 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        final TextView inlog = findViewById(R.id.login);
+        TextView user = findViewById(R.id.user);
+        Button logginbutton = findViewById(R.id.logInButton);
+
+        logginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("===","knapptryckt");
+
+                String log = inlog.getText().toString();
+                Log.d("===","knapptryckt" + log);
+
+
+
+            }
+        });
     }
+
 }
