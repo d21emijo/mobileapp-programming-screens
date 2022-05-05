68ac73580481304ab4b8c8f7bd992126f0b5d094
Emil Jonsson <d21emijo@student.his.se>
2022-05-05 14:48:34 +0200

    initierat variabler

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView login = findViewById(R.id.logIn);
+    Button loginbutton = findViewById(R.id.logInButton);
+    TextView user = findViewById(R.id.user);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("===","knapp tryckt");
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/screens/SecondaryActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.screens;
-
-public class SecondaryActivity {
-}
